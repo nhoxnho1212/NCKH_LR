9f435c5ecf758247ddd67ec8504fe6aad642d0a4
thanhtung <thanhtung@thanhtungs-MacBook-Pro.local>
2020-03-13 01:19:20 +0700

    add init log folder

--- log.py
+++ log.py
@@ -1,7 +1,8 @@
 import logging
 from constants import FILE_LOG
-
+import util.initFolder as initFolder
 def setup_custom_logger(name):
+    initFolder.init('log')
     logger = logging.getLogger(name)
     formatter = logging.Formatter(f'%(asctime)s : %(levelname)s : %(module)s : %(message)s')
     logger.setLevel(logging.DEBUG)
